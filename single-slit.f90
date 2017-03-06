013945b8a756294d319c8e4e3d40b3aa7b4f4e03
Brandon Thornton <brandonthornton10@gmail.com>
2017-03-06 13:06:47 -0700

    Fixed Detection
    Subtracted the incident field for the reflected field measurement. Note
    that Transmission still gets nonphysical contributions in the form of
    boundary reflections from the incident field.

--- single-slit.f90
+++ single-slit.f90
@@ -733,7 +733,7 @@ if((myrank>=0).and.(myrank<(nprocs-1)))then
     Ey(i,j)=Ey(i,j)+dt_eps0*psi_Eyx_2(ii,j)
    endif
   
-  ii = ii-1 
+   ii = ii-1 
   enddo
  enddo
 
@@ -781,8 +781,8 @@ if(myrank==mwR)then
   tmp1=sin(omega_P(nn)*dt*dble(n))
   tmp2=cos(omega_P(nn)*dt*dble(n))
   do i=iw1,iw2
-   Ex_temp(i,nn,1)=Ex_temp(i,nn,1)+tmp1*(Ex(i-1,j)+Ex(i,j))/2.0
-   Ex_temp(i,nn,2)=Ex_temp(i,nn,2)+tmp2*(Ex(i-1,j)+Ex(i,j))/2.0
+   Ex_temp(i,nn,1)=Ex_temp(i,nn,1)+tmp1*(Ex(i-1,j)+Ex(i,j))/2.0 - tmp1*Ex_inc(j) !Take only the reflected field; subtract Inc
+   Ex_temp(i,nn,2)=Ex_temp(i,nn,2)+tmp2*(Ex(i-1,j)+Ex(i,j))/2.0 - tmp2*Ex_inc(j) !Take only the reflected field; subtract Inc
   
    Hz_temp(i,nn,1)=Hz_temp(i,nn,1)+tmp1*(Hz(i-1,j)+Hz(i,j)+Hz(i-1,j-1)+Hz(i,j-1))/4.0
    Hz_temp(i,nn,2)=Hz_temp(i,nn,2)+tmp2*(Hz(i-1,j)+Hz(i,j)+Hz(i-1,j-1)+Hz(i,j-1))/4.0
