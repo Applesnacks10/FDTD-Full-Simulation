60f8a37a5c3ff7a6f50b5c6ddb59b6394f8cac2e
Brandon Thornton <brandonthornton10@gmail.com>
2017-04-02 17:20:56 -0700

    Lingering Neglectant

--- Slit-Groove.f90
+++ Slit-Groove.f90
@@ -46,7 +46,7 @@ integer, parameter :: i0 = 1 + (npml), i1 = Nx - (npml)
 integer, parameter :: mj0=1*2+1,j0=1  !<--- - 590nm
 integer, parameter :: mj1=28*2+2,j1=1!<--- + 500nm
 
-integer, parameter :: ms=29*2+2,js=(21-1)*2+1
+integer, parameter :: ms=29*2+2,js=1
 
 !
 !~~~ Gaussian Source ~~~!
